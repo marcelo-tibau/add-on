6d81ba926f4ee74cf34ab5a5e464c6f1878a931a
Marcelo Tibau <marcelo.tibau@gate.cx>
2017-12-13 19:40:03 -0200

    codes to column chart comparison

---
+++ anotherTestDashTwo.js
@@ -0,0 +1,26 @@
+function doGet(e) {
+  return HtmlService.createTemplateFromFile("index.html")
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+}
+
+function getChartData() {
+    var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1w9in2CLXG2st6uWOPuR6v7YNhPOikfQ6-7oPqtvzS8o/';
+    var sheetName = 'VisaoGeral';
+    var ss = SpreadsheetApp.openByUrl(spreadsheetUrl);
+    var sheet = ss.getSheetByName(sheetName);
+    var headings = sheet.getRange(1,2,1, sheet.getLastColumn()).getValues()[0].map(function(heading) {
+        return heading;
+    });
+    Logger.log(headings);
+    var values = sheet.getRange(2,2, sheet.getLastRow()-3, sheet.getLastColumn()).getValues();
+    var data = [];
+    for (var i=0; i < values.length; i++) {
+        var obj = {};
+        for (var j=0; j < values[i].length; j++) {
+            obj[headings[j]] = values[i][j];
+        }
+        data.push(obj);
+    }
+    return data;
+}
